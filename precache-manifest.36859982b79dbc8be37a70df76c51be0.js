811c7b8e124d06f834f146e666e23a91868b3b11
iph9x <iphnx9@gmail.com>
2020-09-12 21:05:50 +0700

    Updates

---
+++ precache-manifest.36859982b79dbc8be37a70df76c51be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2d835c1648e92cdcb0789382c2bd1e",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "bf527bb1c3970c551d1a",
+    "url": "/todo-react/static/css/main.19019ec9.chunk.css"
+  },
+  {
+    "revision": "4721835484641e214195",
+    "url": "/todo-react/static/js/2.781d8742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.781d8742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf527bb1c3970c551d1a",
+    "url": "/todo-react/static/js/main.3e09b44e.chunk.js"
+  },
+  {
+    "revision": "65cda575b8651f2f0d69",
+    "url": "/todo-react/static/js/runtime-main.dd2d5b3a.js"
+  }
+]);
